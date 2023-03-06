f8d7a1a06ef9f866e16cb5bc0e6433e353461ae4
Xanders <bolulathomas@yahoo.fr>
2023-03-06 05:36:12 +0100

    Update PasswordReset.php

--- app/Models/PasswordReset.php
+++ app/Models/PasswordReset.php
@@ -13,5 +13,5 @@ class PasswordReset extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['email', 'phone', 'token', 'former_password', 'password', 'updated_at'];
+    protected $fillable = ['email', 'phone', 'token', 'former_password', 'updated_at'];
 }
