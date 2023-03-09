0dd6ae6344070fae459a8c56b6edeb712623fdc4
Xanders <bolulathomas@yahoo.fr>
2023-03-09 19:33:08 +0100

    n

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -59,7 +59,7 @@
         <!-- Spinner End -->
 
         <!-- Topbar Start -->
-        <div class="container-fluid bg-dark px-0">
+        <div class="container-fluid bg-primary px-0">
             <div class="row g-0 d-none d-lg-flex">
                 <div class="col-lg-6 ps-5 text-start">
                     <div class="h-100 d-inline-flex align-items-center text-light">
@@ -69,7 +69,7 @@
                     </div>
                 </div>
                 <div class="col-lg-6 text-end">
-                    <div class="h-100 bg-secondary d-inline-flex align-items-center text-dark py-2 px-4">
+                    <div class="h-100 bg-warning d-inline-flex align-items-center text-black py-2 px-4">
                         <span class="me-2 fw-semi-bold"><i class="fa fa-phone-alt me-2"></i>Call Us:</span>
                         <span>+012 345 6789</span>
                     </div>
@@ -77,6 +77,41 @@
             </div>
         </div>
         <!-- Topbar End -->
+
+        <!-- Navbar Start -->
+        <nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top px-4 px-lg-5">
+            <a href="./" class="navbar-brand d-flex align-items-center">
+                <img src="{{ asset('assets/img/logo-text.png') }}" alt="ACR" width="210">
+            </a>
+
+            <button type="button" class="navbar-toggler me-0" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <div class="navbar-nav ms-auto p-4 p-lg-0">
+                    <a href="index.html" class="nav-item nav-link active">Home</a>
+                    <a href="about.html" class="nav-item nav-link">About</a>
+                    <a href="service.html" class="nav-item nav-link">Services</a>
+                    <a href="product.html" class="nav-item nav-link">Products</a>
+                    <div class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
+                        <div class="dropdown-menu bg-light m-0">
+                            <a href="gallery.html" class="dropdown-item">Gallery</a>
+                            <a href="feature.html" class="dropdown-item">Features</a>
+                            <a href="team.html" class="dropdown-item">Our Team</a>
+                            <a href="testimonial.html" class="dropdown-item">Testimonial</a>
+                            <a href="404.html" class="dropdown-item">404 Page</a>
+                        </div>
+                    </div>
+                    <a href="contact.html" class="nav-item nav-link">Contact</a>
+                </div>
+                <div class="border-start ps-4 d-none d-lg-block">
+                    <button type="button" class="btn btn-sm p-0"><i class="fa fa-search"></i></button>
+                </div>
+            </div>
+        </nav>
+        <!-- Navbar End -->
 
         @yield('guest-content')
 
