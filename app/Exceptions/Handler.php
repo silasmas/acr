889fe272a4c1f0c360483f8e6019827c4df14c65
Xanders <1496973+xanderssamoth@users.noreply.github.com>
2023-03-17 04:01:52 +0100

    Update Handler.php

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,8 +2,9 @@
 
 namespace App\Exceptions;
 
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
 {
@@ -44,5 +45,17 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    /**
+     * Display a JSON message if the API user has not authenticated
+     */
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        if ($request->expectsJson()) {
+            return response()->json(['error' => __('notifications.401_description')], 401);
+        }
+
+        return response()->json(['error' => __('notifications.401_description')], 401);
     }
 }
