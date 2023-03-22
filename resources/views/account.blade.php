c6da3fae24d478fc4480c22dd17e90f4c5f30a72
Xanders <1496973+xanderssamoth@users.noreply.github.com>
2023-03-22 15:41:24 +0100

    n

---
+++ resources/views/account.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.guest')
+
+@section('guest-content')
+
+        <!-- About Start -->
+        <div class="container-xxl py-4">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <h1 class="display-1 fw-bold">VOTRE PROFILE</h1>
+                        <h3 class="h3">Espace en construction</h3>
+                        <h3 class="h3">Pour le moment, retournez sur votre appli</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- About End -->
+@endsection
