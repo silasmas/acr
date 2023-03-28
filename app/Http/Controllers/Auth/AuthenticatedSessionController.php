99fd071e191bf49805dc0de6e9dff49cedd290e4
Xanders <1496973+xanderssamoth@users.noreply.github.com>
2023-03-28 18:41:07 +0100

    Update AuthenticatedSessionController.php

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -5,8 +5,6 @@ namespace App\Http\Controllers\Auth;
 use GuzzleHttp\Client;
 use Illuminate\Support\Facades\App;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Auth\LoginRequest;
-use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
