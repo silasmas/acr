a8710dc88b983e027b5b439bb0860d35c173c632
Xanders <1496973+xanderssamoth@users.noreply.github.com>
2023-05-05 04:09:02 +0100

    Update PartyController.php

--- app/Http/Controllers/Web/PartyController.php
+++ app/Http/Controllers/Web/PartyController.php
@@ -38,6 +38,78 @@ class PartyController extends Controller
 
     // ==================================== HTTP GET METHODS ====================================
     /**
+     * GET: Managers list
+     *
+     * @param  $locale
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function managers()
+    {
+        // Select current user API URL
+        $url_user = (!empty($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . '/api/user/' . Auth::user()->id;
+        // Select all countries API URL
+        $url_country = (!empty($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . '/api/country';
+        // Select all received messages API URL
+        $url_message = (!empty($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . '/api/message/inbox/' . Auth::user()->id;
+        // Select all roles API URL
+        $url_roles = (!empty($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . '/api/role';
+        // Select all users by not role API URL
+        $developer_role = 'Manager';
+        $url_manager = (!empty($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . '/api/user/find_by_role/' . $developer_role;
+
+        try {
+            // Select current user API response
+            $response_user = $this::$client->request('GET', $url_user, [
+                'headers' => $this::$headers,
+                'verify'  => false
+            ]);
+            $user = json_decode($response_user->getBody(), false);
+            // Select countries API response
+            $response_country = $this::$client->request('GET', $url_country, [
+                'headers' => $this::$headers,
+                'verify'  => false
+            ]);
+            $country = json_decode($response_country->getBody(), false);
+            // Select all received messages API response
+            $response_message = $this::$client->request('GET', $url_message, [
+                'headers' => $this::$headers,
+                'verify'  => false
+            ]);
+            $messages = json_decode($response_message->getBody(), false);
+            // Select all roles API response
+            $response_roles = $this::$client->request('GET', $url_roles, [
+                'headers' => $this::$headers,
+                'verify'  => false
+            ]);
+            $roles = json_decode($response_roles->getBody(), false);
+            // Select all users by not role API response
+            $response_manager = $this::$client->request('GET', $url_manager, [
+                'headers' => $this::$headers,
+                'verify'  => false
+            ]);
+            $managers = json_decode($response_manager->getBody(), false);
+
+            return view('dashboard.manager', [
+                'current_user' => $user->data,
+                'countries' => $country->data,
+                'messages' => $messages->data,
+                'roles' => $roles->data,
+                'managers' => $managers->data
+            ]);
+
+        } catch (ClientException $e) {
+            // If the API returns some error, return to the page and display its message
+            return view('dashboard.manager', [
+                'current_user' => $user->data,
+                'countries' => $country->data,
+                'messages' => $messages->data,
+                'roles' => $roles->data,
+                'response_error' => json_decode($e->getResponse()->getBody()->getContents(), false)
+            ]);
+        }
+    }
+
+    /**
      * GET: Members list
      *
      * @param  $locale
@@ -174,7 +246,6 @@ class PartyController extends Controller
             $qr_code = QrCode::format('png')->merge((!empty($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . '/assets/img/favicon/android-icon-96x96.png', 0.2, true)->size(135)->generate($member->data->phone);
             // $qr_code = QrCode::size(135)->generate($member->data->phone);
             $message_membre = Notification::where([["user_id",$member->data->id],["notif_name","message"]])->get();
-
 
             return view('dashboard.member', [
                 'current_user' => $user->data,
