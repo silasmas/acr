790702591aafc052ca4597317219f7968dcb6ffd
Xanders <1496973+xanderssamoth@users.noreply.github.com>
2024-01-20 09:55:39 +0100

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -56,7 +56,7 @@ class User extends Authenticatable
 
     /**
      * MANY-TO-MANY
-     * One roles for several users
+     * Several roles for several users
      */
     public function roles()
     {
